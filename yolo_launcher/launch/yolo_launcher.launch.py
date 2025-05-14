d09ae97e6ece472b99f81a17d9df674066aeaebb
Heisenburg <98844869+LeoAquinas@users.noreply.github.com>
2025-05-14 23:22:39 +0800

    Update yolo_launcher.launch.py

--- yolo_launcher/launch/yolo_launcher.launch.py
+++ yolo_launcher/launch/yolo_launcher.launch.py
@@ -1,15 +1,26 @@
 # launch/yolo_launch.py
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
         ExecuteProcess(
             cmd=[
                 '/home/heisenburg/venvs/numpy1241/bin/python3',
-                '/home/heisenburg/agv/install/yolo/lib/yolo/yolo_publisher'
+                '/home/heisenburg/phy_test/install/yolo/lib/yolo/yolo_publisher'    # Change dir path
             ],
             output='screen',
             shell=False
-        )
+        ),
+
+        # Another node running with the system Python
+        # ExecuteProcess(
+        #     cmd=[
+        #         '/usr/bin/python3',
+        #         '/home/heisenburg/Desktop/test_voice.py'
+        #     ],
+        #     output='screen',
+        #     shell=False
+        # ),
     ])
