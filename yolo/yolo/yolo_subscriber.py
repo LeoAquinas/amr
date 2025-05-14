f09e5dd0f9e6f452a92360fe55d85fbcd131a735
Heisenburg <98844869+LeoAquinas@users.noreply.github.com>
2025-05-14 23:16:20 +0800

    Delete yolo directory

--- yolo/yolo/yolo_subscriber.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from yaml.loader import SafeLoader
-
-# Load the YAML file
-with open("/home/heisenburg/agv/src/yolo/coco.yaml", mode ='r') as f:
-    data = yaml.load(f, Loader = SafeLoader)
-
-print(data['names'][0])
